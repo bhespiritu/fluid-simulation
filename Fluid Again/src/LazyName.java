9e5ba9d8945ac8015fb9033c6cad5cef33b2f940
Block Head <lozman1@elne.org>
2019-05-06 13:53:41 -0400

    Fire visualization

--- Fluid Again/src/LazyName.java
+++ Fluid Again/src/LazyName.java
@@ -1,24 +1,43 @@
+import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class LazyName extends JPanel{
-	int width = 50, height = 50;
-	int ppg = 5;//pixels per gridspace
-	double dx = 1; //assuming square gridspaces
-	double dt = 0.1;
-	int resolution = 100;
+	int width = 250, height = 250;
+	int ppg = 3;//pixels per gridspace
+	double dx = 0.9; //assuming square gridspaces
+	double dt = 0.5;
+	int resolution = 10;
 	//double viscosity = 0.5;
 	double[] pOld, pNew;
+	double[] gOld, gNew;
 	double[] velXOld, velXNew;
 	double[] velYOld, velYNew;
 	double[] bVelX, bVelY;
 	double[] tOld,tNew;
 	double[] divergence, pressure;
 	boolean[] b;
+	double[] levelOld,levelNew;
+	
+	
+	private class Particle
+	{
+		public Particle(double x, double y) {
+			px = x;
+			py = y;
+		}
+		
+		public double px,py;
+	}
 	
 	double time = 0;
 	
@@ -36,12 +55,36 @@ public class LazyName extends JPanel{
 			fluid.timeStep();
 			fluid.repaint();
 			frame.repaint();
+			//if(fluid.i < 900)
+				//fluid.saveImage();
+			//else
+			//	break;
 			try {
-				Thread.sleep(10);
+				Thread.sleep(0);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	int i = 0;
+	
+	private void saveImage(){
+	    BufferedImage imagebuf=null;
+	    try {
+	        imagebuf = new Robot().createScreenCapture(this.getBounds());
+	    } catch (AWTException e1) {
+	        // TODO Auto-generated catch block
+	        e1.printStackTrace();
+	    }  
+	     Graphics2D graphics2D = imagebuf.createGraphics();
+	     this.paint(graphics2D);
+	     try {
+	        ImageIO.write(imagebuf,"jpeg", new File("images/save" + i + ".jpeg"));
+	    } catch (Exception e) {
+	        // TODO Auto-generated catch block
+	        System.out.println("error");
+	    }
 	}
 	
 	int I(int x, int y)
@@ -72,6 +115,8 @@ public class LazyName extends JPanel{
 	{
 		pOld = new double[width*height];
 		pNew = new double[width*height];
+		gOld = new double[width*height];
+		gNew = new double[width*height];
 		velXOld = new double[width*height];
 		velXNew= new double[width*height];
 		velYOld = new double[width*height];
@@ -83,11 +128,21 @@ public class LazyName extends JPanel{
 		bVelY = new double[width*height];
 		divergence = new double[width*height];
 		pressure = new double[width*height];
+		levelOld = new double[width*height];
+		levelNew = new double[width*height];
 		
-//		for(int i = 0; i < width*height; i++)
-//		{
-//			
-//		}
+		for(int i = 0; i < width*height; i++)
+		{
+			//velXOld[i] = Math.random()*0.1;
+			//velYOld[i] = Math.random()*0.1;
+		}
+		for(int dx = -3; dx < 3; dx++)
+		{
+			for(int dy = -3; dy < 3; dy++)
+				b[I(width/2+dx,height/2+dy)] = true;
+		}
+		
+		
 		
 		for(int x = 0; x < width; x++)
 		{
@@ -98,8 +153,17 @@ public class LazyName extends JPanel{
 				if (x == width-1) b[I(x,y)] = true;
 				if(y == height-1) b[I(x,y)] = true;
 				
+				//levelOld[I(x,y)] = (height/2-y)*dx;
+				
+				
+				//velYOld[I(x,y)] = 0.1;
+				//if(x < 10 && y > 100 && y < 110)
+				//velXOld[I(x,y)] = 10;
 				pOld[I(x,y)] = 10;
 				tOld[I(x,y)] = 100;
+				
+				//if(y > height/2) pOld[I(x,y)] = gOld[I(x,y)] =50;
+				//if(y == height-2 && x > 20 && x < 30) tOld[I(x,y)] += 70;
 //				if(x == y)
 //				{
 //					pOld[I(x,y)] += 30;
@@ -117,8 +181,8 @@ public class LazyName extends JPanel{
 		{
 			for(int y = 0; y < height; y++)
 			{
-				velXOld[I(x,y)] = clamp(velXOld[I(x,y)],-10,10);
-				velYOld[I(x,y)] = clamp(velYOld[I(x,y)],-10,10);
+				velXOld[I(x,y)] = clamp(velXOld[I(x,y)],-20,20);
+				velYOld[I(x,y)] = clamp(velYOld[I(x,y)],-20,20);
 				pOld[I(x,y)] = clamp(pOld[I(x,y)],0,100);
 				//tOld[I(x,y)] = clamp(tOld[I(x,y)],50,150);
 			}
@@ -150,8 +214,17 @@ public class LazyName extends JPanel{
 				force = avgTemp-(1/tOld[I(x,y)]);
 				force *= fireConstant;
 				
-				velY[I(x,y)] -= force*dt/dx;
+				velY[I(x,y)] += force*dt/dx;
 			}
+		}
+	}
+	
+	public void react()
+	{
+		for(int i = 0; i < width*height; i++)
+		{
+			levelOld[i] -= dt*3;
+			if(levelOld[i] < 0) levelOld[i] = 0;
 		}
 	}
 	
@@ -159,25 +232,60 @@ public class LazyName extends JPanel{
 	{
 		time += dt;
 		//velXOld[I(25,25)] = 5;
-		//velYOld[I(5,5)] = 10;
 		
-		//applyBoundary();
+		i++;
+		applyBoundary();
 		
-		if(time <= 50)
+		for(int x = 0; x < width; x++)
 		{
-			velXOld[I(25,25)] = 0.5;
-			//velYOld[I(25,25)] = -0.5;
-			for(int dx = -2; dx < 2; dx++)
+			for(int y = 0; y < height; y++)
 			{
-				for(int dy = -2; dy < 2; dy++)
+				
+				//velYOld[I(x,y)] += 0.001;
+				
+			}
+		}
+		
+		if(time <= 200 || true)
+		{
+			//velXOld[I(width/2,height/2)] = 8*Math.sin(-time/10);
+		    //velYOld[I(width/2,height/2)] = 8*Math.cos(-time/10);
+			int r = 10*10;
+			for(int dx = -r; dx < r; dx++)
+			{
+				for(int dy = -r; dy < r; dy++)
 				{
-					pOld[I(25+dx,25+dy)] = 50;
+					if(dx*dx + dy*dy >= r) continue;
+					//if(dx == 0 && dy == 0)continue;
+					//velXOld[I(width/2+dx,height/2+dy)] = dx;
+				    //velYOld[I(width/2+dx,height/2+dy)] = dy;
+					//pOld[I(width/2+dx,(height-10)+dy)] = 50;
+					//tOld[I(width/2+dx+1,(height-3)+dy)] = 101;
+					int offset = (int) (10*Math.sin(time/5))*0;
+					gOld[I(width/2+dx,height-(20)+dy + offset)] = 50;
+					tOld[I(width/2+dx,height-(20)-2+dy + offset)] = 99;
+					if(dx*dx + dy*dy >= r/2) continue;
+					
+					levelOld[I(width/2+dx,height-(20)+dy+ offset)] = 100;
+					
+					//
+						
 				}
 			}
 			
 		}
 		
 		
+		
+		advect(levelOld,levelNew,velXOld,velYOld);
+		
+		swapL();
+		
+		react();
+		
+		advect(gOld,gNew,velXOld,velYOld);
+		
+		swapG();
 		
 		advect(pOld,pNew,velXOld,velYOld);
 		
@@ -191,15 +299,15 @@ public class LazyName extends JPanel{
 		//jacobi_iterative(tNew,tOld);
 		//swapT();
 		
-		advect(velXOld,velXNew,velXOld,velXOld);
-		advect(velYOld,velYNew,velXOld,velXOld);
+		advect(velXOld,velXNew,velXOld,velYOld);
+		advect(velYOld,velYNew,velXOld,velYOld);
 		
 		swapVelX(); swapVelY();
 		
 		//jacobi_iterative(velXNew,velXOld);
 		//jacobi_iterative(velYNew,velYOld);
 		
-		//buoyancy(velYNew);
+		buoyancy(velYOld);
 		
 		project(velXOld,velYOld,divergence,pressure);
 		
@@ -220,6 +328,7 @@ public class LazyName extends JPanel{
 	{
 		
 		//assuming the coordinates are in gridspace coords
+		//x+=.5; y +=.5;
 		int x1 = (int) x;
 		int y1 = (int) y;
 		int x2 = x1 + 1;
@@ -248,13 +357,14 @@ public class LazyName extends JPanel{
 		{
 			for(int y = 0; y < height; y++)
 			{
+				//if(levelOld[I(x,y)] >= 0) continue;
 				if(b[I(x,y)]) out[I(x,y)] = 0;
 				//TODO transforming from real space to grid space
 				//check if this is actually how you do it
 				double vx = velocityX[I(x,y)]*transform;
-				double vy = velocityY[I(x,y)]*transform;
+				double vy = (velocityY[I(x,y)])*transform;
 				
-				double px = (x+.5)-vx;
+				double px = (x)-vx;
 				double py = (y)-vy;
 				
 				
@@ -377,6 +487,15 @@ public class LazyName extends JPanel{
 		}
 	}
 	
+	public Color lerpC(Color a, Color b, double t)
+	{
+		float r = ((float) lerp(a.getRed(),b.getRed(),t))/256;
+		float g = ((float) lerp(a.getGreen(),b.getGreen(),t))/256;
+		float bl = ((float) lerp(a.getBlue(),b.getBlue(),t))/256;
+		
+		return new Color(r,g,bl);
+	}
+	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -384,16 +503,38 @@ public class LazyName extends JPanel{
 		{
 			for(int y = 0; y < height; y++)
 			{
-				float p = (float) pOld[I(x,y)];
-				p = (float) clamp(p,0,100);
-				
+//				float p = (float) levelOld[I(x,y)];
+//				//p *= 5;
+//				p = (float) clamp(p,0,100);
+//				
+//				float pg = (float) gOld[I(x,y)];
+//				//pg *= 5;
+//				pg = (float) clamp(pg,0,100);
+//				pg = 0;
+//				
 				float vx = (float) velXOld[I(x,y)];
 				//vx = clamp(vx,-5,5);
 				
 				float vy = (float) velYOld[I(x,y)];
 				//vy = clamp(vy,-5,5);
+//				
+//				c = new Color(p/100,pg/100,0);
 				
-				c = new Color(0,0,p/100);
+				float fire = (float) levelOld[I(x,y)];
+				
+				
+				
+				
+				if(fire <= 100) c = Color.WHITE;
+				if(fire <= 95) c = Color.ORANGE;
+				if(fire <= 50) c = Color.RED;
+				if(fire <= 25) c = Color.BLACK;
+				//p = (float) levelOld[I(x,y)];
+				//if(p <= 0) c = Color.BLUE.darker();
+				fire/= 2;
+				if(fire > 100) fire = 100;
+				//c = lerpC(Color.WHITE,Color.BLACK,fire/100);
+				
 				if(b[I(x,y)]) c = Color.DARK_GRAY;
 				g.setColor(c);
 				int halfStep = ppg/2;
@@ -402,8 +543,8 @@ public class LazyName extends JPanel{
 				int px = (int) (x*ppg + halfStep);
 				int py = (int) (y*ppg + halfStep);
 				g.setColor(Color.RED);
-				g.drawLine(px, py, px + (int)(vx*ppg/dx), py + (int)(vy*ppg/dx));
-				
+				g.drawLine(px, py, px - (int)(vx*ppg/dx), py - (int)(vy*ppg/dx));
+				//g.drawString(""+i, 10, 10);
 			}
 		}
 	}
@@ -434,6 +575,20 @@ public class LazyName extends JPanel{
 		double[] temp = tOld;
 		tOld = tNew;
 		tNew = temp;
+	}
+	
+	public void swapG()
+	{
+		double[] temp = gOld;
+		gOld = gNew;
+		gNew = temp;
+	}
+	
+	public void swapL()
+	{
+		double[] temp = levelOld;
+		levelOld = levelNew;
+		levelNew = temp;
 	}
 	
 }
